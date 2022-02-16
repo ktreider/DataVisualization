f48c03de3b671304c5d7508c1f90481c0e9391a1
Katie Reider <ktreider5@gmail.com>
2022-02-15 20:18:28 -0500

    entropy and info gain, session 7

--- code/questions/questions6_entropyAndInformationGain.R
+++ code/questions/questions6_entropyAndInformationGain.R
@@ -32,16 +32,20 @@ plot(tr)
 
 #WANT: IG(parent, age), IG(parent, job), IG(parent, house), IG(parent, credit)
 
-#code followed from https://rstudio-pubs-static.s3.amazonaws.com/455435_30729e265f7a4d049400d03a18e218db.html
+#code/calculations followed from https://rstudio-pubs-static.s3.amazonaws.com/455435_30729e265f7a4d049400d03a18e218db.html
 
 
 #compute entropy
 entropy <- function(target) {
+  
   freq <- table(target)/length(target)
+  
   #vectorize
   vec <- as.data.frame(freq)[,2]
+
   #drop 0 to avoid NaN resulting from log2
-  vec<-vec[vec>0]
+  vec<-vec[vec>0]  
+  
   #compute entropy
   -sum(vec * log2(vec))
 }
@@ -63,16 +67,41 @@ infoGain <-function(data, feature, target){
   parent_ent <- entropy(data[,target])
   #calculate p for each value of feature
   cleaned_data$p <- cleaned_data$n/nrow(data)
+ 
+  #troubleshooting
+  cat("entropies:", cleaned_data$e, "\n")
+  cat("probabilities:", cleaned_data$p, "\n")
+  formula <- sum(cleaned_data$p*cleaned_data$e)
+  print(paste("sum(p*e):", formula))
+  print(paste("parent entropy:", parent_ent))
+  
   #compute IG
   IG <- parent_ent - sum(cleaned_data$p*cleaned_data$e)
   
-  return(IG)
+  return(print(IG))
 }
-infoGain(loanDf, 'age', 'loan')
-infoGain(loanDf, 'job', 'loan')
-infoGain(loanDf, 'house', 'loan')
-infoGain(loanDf, 'credit', 'loan')
 
+#----------------------------------
+age <- infoGain(loanDf, 'age', 'loan')
+
+#----------------------------------
+job <- infoGain(loanDf, 'job', 'loan')
+
+#----------------------------------
+house <- infoGain(loanDf, 'house', 'loan')
+
+#----------------------------------
+credit <- infoGain(loanDf, 'credit', 'loan')
+
+
+
+#----------------------------------
+cat("final infomation gains:\n", 
+    "IG(parent, age) = ", age, "\n",
+    "IG(parent, job) = ", job, "\n",
+    "IG(parent, house) = ", house, "\n",
+    "IG(parent, credit) = ", credit, "\n"
+    )
 
 
 
--- code/session7Feb15.R
+++ code/session7Feb15.R
@@ -1,1 +1,52 @@
 #session 7
+
+#---control structures---------------------------------------------------------
+
+#if (condition) {
+#instructions
+#}
+
+#if else
+#while loops
+#for loops
+#switch
+
+#simplified ifelse statement:
+#ifelse(condition, ifTRUE, ifFALSE)
+
+#---ways to print--------------------------------------------------------------
+x <- 5.123
+sprintf("The number is: %1.0f", x)
+#%s string, %f doubles
+
+# error: print("Hello", "There")
+print(paste("Hello", "There", 55))
+
+
+cat("Hi", "Bye", 40)
+
+
+
+
+#---example function-----------------------------------------------------------
+stockOpt <- function(stockStatus) {
+  if(stockStatus == "down") {
+    return("Sell")
+  }
+}
+opt <- stockOpt("down")
+print(opt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
